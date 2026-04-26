74237e404e95328ec8386f2f98784cce969f1400
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-04-26 18:41:58 +0300

    feat(spectogram): added spectogram code to the feature extraction src

--- src/feature_extraction/feature_extraction.py
+++ src/feature_extraction/feature_extraction.py
@@ -32,3 +32,42 @@ def mfcc_stats_with_deltas_torch(mfcc):
         delta.mean(dim=1), delta.std(dim=1),
         delta2.mean(dim=1), delta2.std(dim=1)
     ])
+
+
+def extract_spectrogram_torch(audio: torch.Tensor, n_fft=1024, hop_length=512):
+    # audio shape must be [1, T]
+    if audio.dim() == 1:
+        audio = audio.unsqueeze(0)
+
+    transform = torchaudio.transforms.Spectrogram(
+        n_fft=n_fft,
+        hop_length=hop_length,
+        power=2.0
+    )
+
+    S = transform(audio)  # [1, freq_bins, time]
+
+    S_db = torchaudio.transforms.AmplitudeToDB()(S)
+
+    return S_db.squeeze(0)  # [freq_bins, time]    
+
+
+def extract_mel_spectrogram_torch(audio: torch.Tensor, sr: int,
+                                   n_fft=1024, hop_length=512, n_mels=40):
+
+    if audio.dim() == 1:
+        audio = audio.unsqueeze(0)
+
+    mel_transform = torchaudio.transforms.MelSpectrogram(
+        sample_rate=sr,
+        n_fft=n_fft,
+        hop_length=hop_length,
+        n_mels=n_mels,
+        power=2.0
+    )
+
+    mel = mel_transform(audio)
+
+    mel_db = torchaudio.transforms.AmplitudeToDB()(mel)
+
+    return mel_db.squeeze(0)  # [n_mels, time]
